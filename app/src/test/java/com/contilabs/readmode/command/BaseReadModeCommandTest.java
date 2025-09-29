dad364c32b57ff472c224b49cc34379a1ee6ce99
Alan Quintero <alan_q_b@hotmail.com>
2025-09-28 23:41:32 -0600

    Add missing annotation

--- app/src/test/java/com/contilabs/readmode/command/BaseReadModeCommandTest.java
+++ app/src/test/java/com/contilabs/readmode/command/BaseReadModeCommandTest.java
@@ -14,10 +14,13 @@ import com.contilabs.readmode.model.ReadModeSettings;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class BaseReadModeCommandTest extends BaseTest {
 
     @Mock
